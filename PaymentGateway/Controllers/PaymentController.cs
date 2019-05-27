7fe2f244e905e15fe99280e77879a784f05d1444
Faadil <muhammad.faadil.garda-sahib@elca.mu>
2019-05-27 15:01:30 +0400

    arch

--- PaymentGateway/Controllers/PaymentController.cs
+++ PaymentGateway/Controllers/PaymentController.cs
@@ -7,11 +7,22 @@ using Swashbuckle.AspNetCore.SwaggerGen;
 using PaymentGateway.Core;
 using PaymentGateway.Core.Entities;
 using PaymentGateway.Core.Requests;
+using PaymentGateway.Infrastructure;
 
 namespace PaymentGateway.Controllers
 {
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
     public class PaymentController : ControllerBase
     {
+        private readonly PaymentDbContext _paymentDbContext;
+
+        public PaymentController(PaymentDbContext paymentDbContext)
+        {
+            this._paymentDbContext = paymentDbContext;
+        }
+
         [SwaggerOperation(operationId: "GetPayment")]
         [HttpGet("", Name = "GetPayment")]
         [ProducesResponseType(typeof(Payment), 200)]
